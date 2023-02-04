7050a536648c42814710a0c438cddd562b40aebb
sequoyah1 <sequoyahschaefer@yahoo.com>
2023-02-04 02:32:12 -0500

    Completed lab 3 Sequoyah Schaefer

--- src/GettingHotInHere.java
+++ src/GettingHotInHere.java
@@ -1,3 +1,9 @@
+/**
+ * @author Trevor Hartman
+ * @author Sequoyah Schaefer
+ * @since Version 1.0
+ */
+
 import java.util.Scanner;
 
 public class GettingHotInHere {
@@ -10,5 +16,22 @@ public class GettingHotInHere {
         Scanner scanner = new Scanner(System.in);
         System.out.print("What is the temperature in °F: ");
         int number = scanner.nextInt();
+        float Fahrenheit, Celsius;
+
+            Fahrenheit = number;
+        Celsius = ((Fahrenheit-32)*5)/9;
+        System.out.println(number + "\u00B0" + "F");
+        System.out.printf("%.6f",((Fahrenheit-32)*5)/9);
+        System.out.print("\u00B0" + "C");
+        System.out.printf("\n" + "%.1f",((Fahrenheit-32)*5)/9);
+        System.out.println("\u00B0" + "C" );
+        System.out.printf("If it were 2" + "\u00B0" + "C" + " " + "warmer it would be:" + "  " + "%.6f", ((Fahrenheit-32)*5)/9 + 2) ;
+
+
+
+
+
+
     }
+
 }
--- src/MrRoboto.java
+++ src/MrRoboto.java
@@ -1,3 +1,8 @@
+/**
+ * @author Trevor Hartman
+ * @author Sequoyah Schaefer
+ * @since Version 1.0
+ */
 // Import the Scanner tool used for reading (All external packages/libraries/apis are imported similarly this)
 import java.util.Scanner;
 
@@ -9,11 +14,12 @@ public class MrRoboto {
     public static void main(String[] args) {
         // The instantiation code for a Scanner instance.
         Scanner scanner = new Scanner(System.in);
-
+        String message = scanner.nextLine();
         // Concatenate the user response to this String.
         String domo = "Domo arigato ";
-
-        System.out.print("My name is Mr. Roboto, are you Kilroy? ");
+        String stan = "no, I'm stan";
+        System.out.print("My name is Mr.Roboto, are you Kilroy?" + "\s");
+        System.out.print(stan + "\n" + domo + stan );
         // Write your program here
     }
 }
